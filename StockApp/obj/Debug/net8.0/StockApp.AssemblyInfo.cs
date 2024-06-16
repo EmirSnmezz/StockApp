4cc5254168c1dc825036d855ee95c75bd07d5bfa
emir <emircan_snmez@outlook.com>
2024-06-17 02:43:25 +0300

    gelştirilmeye devam edilecek

--- StockApp/obj/Debug/net8.0/StockApp.AssemblyInfo.cs
+++ StockApp/obj/Debug/net8.0/StockApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf451394f78dafba0ed55906672540b34f3a00d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46837174305b3443d8a0548a8eda98e9cdef0f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
